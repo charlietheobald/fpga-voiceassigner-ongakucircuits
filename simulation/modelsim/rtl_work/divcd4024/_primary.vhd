61f5ea110e1b50c5dfb4a945a702228be32f65ab
Charlie Theobald OneDrive <charlie.theobald.home@gmail.com>
2026-09-10 01:22:23 +0100

    Attempt at removing rubbish that doesn't need to be on here

--- simulation/modelsim/rtl_work/divcd4024/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity divcd4024 is
-    port(
-        clkin           : in     vl_logic;
-        rst             : in     vl_logic;
-        \out\           : out    vl_logic_vector(6 downto 0)
-    );
-end divcd4024;
